089b77c419fb021d913bc7d5653004379774f1e3
Damiano Ficara <damianoficara@gmail.com>
2023-10-18 15:21:02 +0000

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,14 +1,12 @@
 from setuptools import setup, find_packages
-setup(name='prova',
-      version='0.1.0',
-      description='Some Python package',
-      author='Me',
-      author_email='me@example.com',
-      license='MIT',
-      packages=["application", "database"],
-      package_dir={
-            "": ".",
-            "application": "./application",
-            "database": "./database",
-      },
-      zip_safe=False)
+
+setup(
+    name='prova',
+    version='0.1.0',
+    description='Some Python package',
+    author='Me',
+    author_email='me@example.com',
+    license='MIT',
+    packages=find_packages(),  # Utilizza find_packages() per trovare automaticamente i pacchetti
+    zip_safe=False
+)
