9f61f1e5d291d8781adbadd30f6eeba397f73f87
Damiano Ficara <damianoficara@gmail.com>
2023-10-19 15:34:57 +0000

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,5 +1,5 @@
 from setuptools import setup, find_packages
-setup(name='prova',
+setup(name='provapipeline2023',
       version='0.1.0',
       description='Some Python package',
       author='Me',
@@ -12,3 +12,4 @@ setup(name='prova',
             "database": "./database",
       },
       zip_safe=False)
+
