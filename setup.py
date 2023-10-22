004e4ce71ff548ce0b39fd0cbef4c3d90a6d0f3d
Damiano <damianoficara@gmail.com>
2023-10-23 00:57:05 +0200

    add some changes

--- setup.py
+++ setup.py
@@ -1,9 +1,19 @@
 from setuptools import setup, find_packages
-setup(name='provapipeline2023',
-      version='0.1.1',
-      description='Some Python package',
-      author='Me',
-      author_email='me@example.com',
+
+with open("README.md", "r") as f:
+      long_description = f.read()
+
+author_emails = [
+    'damianoficara@gmail.com',
+    # Aggiungi altre email se necessario
+]
+
+setup(name='2023_assignemt1_viewscounter',
+      version='0.0.1',
+      description='This assignment focuses on creating an application that counts and monitors user views',
+      author='Team CED',
+      author_email=author_emails,
+      long_description=long_description,
       license='MIT',
       packages=["application", "database"],
       package_dir={
@@ -12,4 +22,3 @@ setup(name='provapipeline2023',
             "database": "./database",
       },
       zip_safe=False)
-
