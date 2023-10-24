7e9bce6c3b0c93fe28803fcfd24c644d87602d25
Damiano Ficara <damianoficara@gmail.com>
2023-10-24 20:01:00 +0000

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,20 +1,18 @@
 from setuptools import setup, find_packages
+from setuptools_scm import get_version
 
-with open("README.md", "r") as f:
-      long_description = f.read()
+with open("README.md", "r", encoding="utf-8") as f:
+    long_description = f.read()
 
-
-setup(name='2023_assignemt1_viewscounter',
-      version='0.0.3',
-      description='This assignment focuses on creating an application that counts and monitors user views',
-      author='Team CED',
-      author_email='damianoficara@gmail.com,c.ricci19@campus.unimib.it,emiliotoli21@gmail.com',
-      long_description=long_description,
-      license='MIT',
-      packages=["application", "database"],
-      package_dir={
-            "": ".",
-            "application": "./application",
-            "database": "./database",
-      },
-      zip_safe=False)
+setup(
+    name='2023_assignemt1_viewscounter',
+    version="0.0.1",
+    description='Questo assignment si concentra sulla creazione di un\'applicazione che conta e monitora le visualizzazioni degli utenti',
+    author='Team CED',
+    author_email='damianoficara@gmail.com,c.ricci19@campus.unimib.it,emiliotoli21@gmail.com',
+    long_description=long_description,
+    long_description_content_type='text/markdown',  
+    license='MIT',
+    packages=find_packages(exclude=["tests"]),
+    zip_safe=False
+)
