00dfd169250491683cffe38ad15ba863b80cc10d
Damiano Ficara <damianoficara@gmail.com>
2023-10-24 20:07:31 +0000

    Update setup.py

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", "r", encoding="utf-8") as f:
 
 setup(
     name='2023_assignemt1_viewscounter',
-    version="0.0.1",
+    version="0.0.2",
     description='Questo assignment si concentra sulla creazione di un\'applicazione che conta e monitora le visualizzazioni degli utenti',
     author='Team CED',
     author_email='damianoficara@gmail.com,c.ricci19@campus.unimib.it,emiliotoli21@gmail.com',
