d6f6a4ac3fcd34c839b8a413627fcb5ac7c8634a
damiano013 <damianoficara@gmail.com>
2023-11-02 13:44:51 +0000

    incremento versione

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", "r", encoding="utf-8") as f:
 
 setup(
     name='2023_assignment1_viewscounter',
-    version='0.0.7',
+    version='0.0.8',
     description='Questo assignment si concentra sulla creazione di un\'applicazione che conta e monitora le visualizzazioni degli utenti',
     author='Team CED',
     author_email='damianoficara@gmail.com,c.ricci19@campus.unimib.it,emiliotoli21@gmail.com',
